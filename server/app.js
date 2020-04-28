c243de8ccf14771ce2e857360a3ba37398f2ef0d
jaybenaim <benaimjacob@gmail.com>
2020-04-28 18:36:37 -0400

    used build folder for serving angular instead of dist

--- server/app.js
+++ server/app.js
@@ -30,8 +30,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.resolve(__dirname, "build")));
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(__dirname + "/dist/"));
 
 app.use("/api", indexRouter);
 app.get("*", (req, res) => {
